066f99dc6e2b54b859ead2f94ab3f2940cc48023
zorankrog-droid <zoran.krog@gmail.com>
2026-05-14 04:31:07 +0200

    Fix PWA build

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,13 @@
 import withPWA from "next-pwa";
 
-const isDev =
-  process.env.NODE_ENV ===
-  "development";
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
 };
 
 export default withPWA({
   dest: "public",
   disable: isDev,
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "NetworkFirst",
-      options: {
-        cacheName:
-          "offlineCache",
-        expiration: {
-          maxEntries: 200,
-        },
-      },
-    },
-  ],
 })(nextConfig);
