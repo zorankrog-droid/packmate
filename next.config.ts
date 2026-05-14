005907828e2ce4203bc1345b6d9439de85ebb0f1
zorankrog-droid <zoran.krog@gmail.com>
2026-05-14 04:27:57 +0200

    Add offline cache

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,29 @@
 import withPWA from "next-pwa";
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev =
+  process.env.NODE_ENV ===
+  "development";
 
 const nextConfig = {
-  turbopack: {},
+  reactStrictMode: true,
 };
 
 export default withPWA({
   dest: "public",
   disable: isDev,
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName:
+          "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 })(nextConfig);
